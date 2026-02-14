c41e73647ccf75a773dcd5b7a546e0fadb82f477
Yah-oo5726 <arsalannimaamini@gmail.com>
2026-02-13 16:50:08 -0800

    Fixed bug relating to when player is drawn causing it to not appear

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
         player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main()
