24fd2e01a1e4256be5a1f2baf00f3aac068c9114
Yah-oo5726 <arsalannimaamini@gmail.com>
2026-02-13 16:49:11 -0800

    Instantiated player in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
@@ -8,6 +9,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
         screen.fill("black")
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        player.draw(screen)
 
 if __name__ == "__main__":
     main()
